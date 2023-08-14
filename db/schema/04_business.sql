c65f60083f7ae764289480f4733dae45f8c04659
@OneJuicyDoor <shephardcam@gmail.com>
2023-08-14 12:47:28 -0300

    consolodate all the schema and seeds into two files for ease of use

--- db/schema/04_business.sql
+++
@@ -1,6 +0,0 @@
-DROP TABLE IF EXISTS business CASCADE;
-CREATE TABLE business (
-  id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255),
-  description VARCHAR(1000)
-);
--- db/seeds/02_addresses_seeds.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO addresses (customer_id, bussines_id, street_name, street_number, city)
-VALUES
-    (1, NULL, 'Main Street', 123, 'Cityville'),
-    (2, NULL, 'Elm Avenue', 456, 'Townsville'),
-    (NULL, 1, 'Oak Street', 789, 'Villagetown'),
-    (NULL, 2, 'Maple Drive', 987, 'Metropolis'),
-    (3, NULL, 'Cedar Road', 234, 'Hamletville');
