364669d77e7ceb86379ecd591900e60460a849c5
Leventbk <leventbkarakus@hotmail.com>
2023-08-18 12:59:52 -0400

    3 address are passived

--- db/seeds/03_business_adresses.sql
+++ db/seeds/03_business_adresses.sql
@@ -4,6 +4,6 @@ INSERT INTO business_addresses ( business_id, street_name, street_number, city)
 VALUES
     ( 1, 'Main Street', 123, 'Cityville'),
     ( 2, 'Elm Avenue', 456, 'Townsville'),
-    ( 1, 'Oak Street', 789, 'Villagetown'),
-    ( 2, 'Maple Drive', 987, 'Metropolis'),
-    ( 2, 'Cedar Road', 234, 'Hamletville');
+    -- ( 1, 'Oak Street', 789, 'Villagetown'),
+    -- ( 2, 'Maple Drive', 987, 'Metropolis'),
+    -- ( 2, 'Cedar Road', 234, 'Hamletville');
