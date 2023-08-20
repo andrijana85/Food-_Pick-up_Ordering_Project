0181d79b95edb32417c92389151f8dd9b3400036
andrijana85 <topalovic.andrijana@yahoo.com>
2023-08-20 19:52:42 -0400

    add /menu route

--- routes/api-items.js
+++ routes/api-items.js
@@ -9,10 +9,24 @@ const express = require('express');
 const router  = express.Router();
 const db = require('../db/queries/items');
 
+//test this route
 router.get('/:ownerId', (req, res) => {
   //call the itemQueries.getItems(owner_id)
   const ownerId = req.params.id;
   db.getItems(ownerId)
+    .then(items => {
+      res.json({ items });
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: err.message });
+    });
+});
+
+//done
+router.get('/', (req, res) => {
+  db.getItems()
     .then(items => {
       res.json({ items });
     })
