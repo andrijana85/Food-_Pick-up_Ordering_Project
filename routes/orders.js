3f879e4477d8eabe8b91c4e23f30b69c3c676cef
Leventbk <leventbkarakus@hotmail.com>
2023-08-19 20:13:24 -0400

    sendTextMessage

--- routes/orders.js
+++ routes/orders.js
@@ -1,16 +1,18 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 const db = require('../db/queries/orders');
+const { sendTextMessage } = require('../twilio/twilio');
+// TODO: message response from twilio
 
 router.get('/', (req, res) => {
-    db.getOrders()
-      .then(orders => {
-        res.json(orders);
-      })
-      .catch(error => {
-        console.log(error.message);
-        res.status(500).json({err:"internal error"});
-      });
-  });
+  db.getOrders()
+    .then(orders => {
+      res.json(orders);
+    })
+    .catch(error => {
+      console.log(error.message);
+      res.status(500).json({ err: "internal error" });
+    });
+});
 
-  module.exports = router;
+module.exports = router;
