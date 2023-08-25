d5cfd20a7f89cf22a0c6c71a67edde5ab5e4e00f
@OneJuicyDoor <shephardcam@gmail.com>
2023-08-25 14:58:41 -0300

    crated css for menu and orders

--- public/scripts/menu.js
+++ public/scripts/menu.js
@@ -110,9 +110,11 @@ const renderTotal = function(total) {
 
 const createItemElement = function(item) {
   const element = $(`
-  <img src=${item.image_url} width = "80px" height = "80px"> 
-  <li class="foodItem" id=${item.id}>${item.name} $${item.price} </li> 
-  <button class="add-item" id=${item.id}>Add to the cart</button>
+  <div class="menu-item">
+  <img src=${item.image_url} width="80px" height="80px"> 
+  <li class="foodItem" id=${item.id}>${item.name} $${item.price}</li>
+</div>
+<button class="add-item" data-item=${JSON.stringify(item)}>Add to the cart</button>
   `);
   element.data("item", item);
   return element;
